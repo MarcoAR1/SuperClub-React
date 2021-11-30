1d98690d00a3dd57e98d1e2f8277f6590fd1db22
Mateo Troilo <mateo.troilo@alumnos.frgp.utn.edu.ar>
2021-11-30 17:14:24 -0300

    implement products list

--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import './ProductsList.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 const ProductsList = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  let [products, setProducts] = useState([])
+
+  useEffect(() => {
+    ;(async function () {
+      let { data } = await axios.get(
+        'https://dhfakestore2.herokuapp.com/api/products/'
+      )
+      setProducts(data)
+      console.log(data)
+    })()
+  }, [])
+
+  return (
+    <section className="section-products">
+      {products.map((product, i) => (
+        <Link className="links-products" to={'/products/' + product._id}>
+          <article className="article-product" key={i}>
+            <div className="container-products-info">
+              <img className="article-img" src={product.image} alt="" />
+              <div>
+                <p>{product.title}</p>
+                <p>#{product._id}</p>
+              </div>
+            </div>
+            <img
+              className="btn-product"
+              src="/assets/chevron-right (1).svg"
+              alt=""
+            />
+          </article>
+        </Link>
+      ))}
+    </section>
+  )
 }
 
 export default ProductsList
