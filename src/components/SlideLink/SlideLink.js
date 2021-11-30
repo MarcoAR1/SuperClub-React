d9fadcf8f6b2c3951803d9040cfe2b47f349afd1
AgusB1101 <agustinbernal69@gmail.com>
2021-11-30 18:06:29 -0300

    Implement selected SlideLink when window loads

--- src/components/SlideLink/SlideLink.js
+++ src/components/SlideLink/SlideLink.js
@@ -3,6 +3,14 @@ import { useNavigate } from 'react-router-dom'
 import './SlideLink.css'
 
 const SlideLink = ({ image, handlerAllSlideLinks, link, children }) => {
+  window.addEventListener('load', () => {
+    const location = window.location.pathname
+    if (location === link)
+      slideLink.current?.classList.add(
+        isDark ? 'darkSlideLinkSelected' : 'slideLinkSelected'
+      )
+  })
+
   const slideLink = useRef()
   const navigate = useNavigate()
 
