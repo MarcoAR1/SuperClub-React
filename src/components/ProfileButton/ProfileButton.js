5adf80f983ef1fc6bd78eeb0e4e08b1ac658037e
AgusB1101 <agustinbernal69@gmail.com>
2021-11-30 18:17:00 -0300

    Changed button for div in profile and fixed side bar view

--- src/components/ProfileButton/ProfileButton.js
+++ src/components/ProfileButton/ProfileButton.js
@@ -1,16 +1,23 @@
+import { useNavigate } from 'react-router'
 import Button from '../Button/Button'
 import './ProfileButton.css'
 
 const ProfileButton = ({ name }) => {
+  const navigate = useNavigate()
+
+  const handlerProfileBtn = () => {
+    navigate('/profile')
+  }
+
   return (
-    <Button classNameButton="profileBtn">
+    <div className="profileBtn" onClick={handlerProfileBtn}>
       <img
         className="profileImg"
         src="/assets/profile-pic.png"
         alt="Imagen de perfil"
       />
       {name}
-    </Button>
+    </div>
   )
 }
 
