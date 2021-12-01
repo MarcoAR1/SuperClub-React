0bfc936b1bc1209eb9ec1fab6b5a1d95558958ba
MarcoAR <marcorivero.mr26@gmail.com>
2021-12-01 12:08:18 -0300

    adapter filter to generic

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react'
 
-const useFilter = () => {
+const useFilter = ({
+  textFilter = 'name',
+  secontText = 'title',
+  selectFilter = 'category'
+} = {}) => {
   const [text, setText] = useState('')
   const [select, setSelect] = useState('')
   const [element, setElement] = useState([])
@@ -11,9 +15,13 @@ const useFilter = () => {
       setElement(() => {
         const filterElement = storageElement?.filter((elm) => {
           const fixedText = text.toLowerCase().trim()
-          const categoryExist = select ? elm.category === select : true
-          const startNameWithText = elm?.name?.toLowerCase().match(fixedText)
-          const startTitleWithText = elm?.title?.toLowerCase().match(fixedText)
+          const categoryExist = select ? elm[selectFilter] === select : true
+          const startNameWithText = elm[textFilter]
+            ?.toLowerCase()
+            .match(fixedText)
+          const startTitleWithText = elm[secontText]
+            ?.toLowerCase()
+            .match(fixedText)
           return categoryExist && (startNameWithText || startTitleWithText)
         })
         return filterElement
