8dfe3b38525959f6d6997a8fd5e75789f6a3328b
MateoTroilo <mateotroilo@gmail.com>
2021-12-01 12:26:36 -0300

    Implement Elsa for Loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,14 +1,11 @@
 import React from 'react'
-import './Loader.css'
+import Elsa from '../Elsa/Elsa'
 
 const Loader = () => {
   return (
-    <div className="message">
-      <img className="notFoundImg" src="/assets/elsa.gif" alt="Elsa" />
-      <h2 className="notFoundTitle">
-        Cargando... <br /> Espera unos momentos
-      </h2>
-    </div>
+    <Elsa>
+      Cargando... <br /> Espera unos momentos
+    </Elsa>
   )
 }
 
