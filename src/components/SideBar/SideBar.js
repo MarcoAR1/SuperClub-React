a52dd7a399e10db9570d2a5215171931666d9385
MarcoAR <marcorivero.mr26@gmail.com>
2021-12-01 21:34:12 -0300

    fix main area scroll bar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -24,53 +24,58 @@ const SideBar = React.forwardRef(({ closeMenu }, ref) => {
     e.stopPropagation()
   }
   return (
-    <div
-      ref={ref}
-      className="sideBar"
-      tabIndex="1"
-      onClick={(e) => handlerSideBar(e)}
-      onBlur={closeMenu}
-    >
-      <div className="logoSlideLinksContainer">
-        <img
-          src="/assets/Banco_Santander_Logotipo.svg"
-          alt="Logo"
-          className="logo"
-        />
-        <div ref={slideLinks} className="slideLinks">
-          <SlideLink
-            handlerAllSlideLinks={handlerAllSlideLinks}
-            link="/"
-            image="/assets/home.svg"
-            isDark={isDark}
-          >
-            Inicio
-          </SlideLink>
-          <SlideLink
-            handlerAllSlideLinks={handlerAllSlideLinks}
-            link="/products"
-            image="/assets/package-variant-closed.svg"
-            isDark={isDark}
-          >
-            Productos
-          </SlideLink>
-          <SlideLink
-            handlerAllSlideLinks={handlerAllSlideLinks}
-            link="/stores"
-            image="/assets/store.svg"
-            isDark={isDark}
-          >
-            Tiendas
-          </SlideLink>
+    <>
+      <div
+        ref={ref}
+        className="sideBar"
+        tabIndex="1"
+        onClick={(e) => handlerSideBar(e)}
+        onBlur={closeMenu}
+      >
+        <div className="logoSlideLinksContainer">
+          <img
+            src="/assets/Banco_Santander_Logotipo.svg"
+            alt="Logo"
+            className="logo"
+          />
+          <div ref={slideLinks} className="slideLinks">
+            <SlideLink
+              handlerAllSlideLinks={handlerAllSlideLinks}
+              link="/"
+              image="/assets/home.svg"
+              isDark={isDark}
+            >
+              Inicio
+            </SlideLink>
+            <SlideLink
+              handlerAllSlideLinks={handlerAllSlideLinks}
+              link="/products"
+              image="/assets/package-variant-closed.svg"
+              isDark={isDark}
+            >
+              Productos
+            </SlideLink>
+            <SlideLink
+              handlerAllSlideLinks={handlerAllSlideLinks}
+              link="/stores"
+              image="/assets/store.svg"
+              isDark={isDark}
+            >
+              Tiendas
+            </SlideLink>
+          </div>
         </div>
+        <ProfileButton
+          name="Olivia"
+          link="/profile"
+          handlerAllSlideLinks={handlerAllSlideLinks}
+          isDark={isDark}
+        />
       </div>
-      <ProfileButton
-        name="Olivia"
-        link="/profile"
-        handlerAllSlideLinks={handlerAllSlideLinks}
-        isDark={isDark}
-      />
-    </div>
+      <div
+       className='sliderBarPlaceHolder'
+      ></div>
+    </>
   )
 })
 
